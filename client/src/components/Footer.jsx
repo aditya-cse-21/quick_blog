32fecde733aa38bdf1992767d8d7c02a3acd641f
Aditya <151380656+aditya-cse-21@users.noreply.github.com>
2025-07-20 23:36:49 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © QuickBlog - All Right Reserved.</p>
+      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © QuickBlog - Made By Aditya.</p>
     </div>
   )
 }
