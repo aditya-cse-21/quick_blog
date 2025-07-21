1469d3ac121711807256ff1e3d59a16cf114e669
Aditya <151380656+aditya-cse-21@users.noreply.github.com>
2025-07-22 00:42:39 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log('MongoDB connected successfully'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickBlog`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)
     } catch (error) {
         console.log(error.message);
     }
